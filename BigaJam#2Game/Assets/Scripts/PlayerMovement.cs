e5bbe60a8b540d6e035e870fffe88bc9344af4bf
GoodDeveloper-coder <itmancik@gmail.com>
2023-11-26 19:51:50 +0200

    Revert "Merge branch 'ShootingSystem'"
    This reverts commit 6154e30bbc2541e222ad3fbff075e6015365d895, reversing
    changes made to b5ab3f2dffb99206bbdd6bec5fcce8f0335cfa09.

--- BigaJam#2Game/Assets/Scripts/PlayerMovement.cs
+++ BigaJam#2Game/Assets/Scripts/PlayerMovement.cs
@@ -13,7 +13,6 @@ public class PlayerMovement : MonoBehaviour
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
-        //_gunScript.SetPlayer(PlayerIndex);
     }
     private void Awake() 
     {
@@ -45,9 +44,7 @@ public class PlayerMovement : MonoBehaviour
     {
         if ((moveVectorFirst.x > 0 && !faceRight) || (moveVectorFirst.x < 0 && faceRight))
         {
-            if (faceRight) GetComponent<SpriteRenderer>().flipX = true;
-            if (!faceRight) GetComponent<SpriteRenderer>().flipX = false;
-            //transform.localScale *= new Vector2(-1, 1);
+            transform.localScale *= new Vector2(-1, 1);
             faceRight = !faceRight;
         }
     }
